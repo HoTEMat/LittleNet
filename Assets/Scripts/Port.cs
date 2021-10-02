ee6093c6231edf375f02ebbb8e716307f7772c7a
Jan Kytka <kytka.honza@gmail.com>
2021-10-02 16:09:18 +0200

    add GetNeighbors

--- Assets/Scripts/Port.cs
+++ Assets/Scripts/Port.cs
@@ -1,18 +1,62 @@
 
-class Port : IPort {
-    private IGrid Grid;
+using System.Diagnostics;
+using System.Linq;
 
-    public Port(IGrid grid, int x, int y) {
-        Grid = grid;
+class Port : IPort {
+    private readonly IGridContainer Container;
+    private readonly IGrid Grid;
+
+    public Port(IGridContainer cont, int x, int y) {
+        Container = cont;
+        Grid = cont.Grid;
 
         InnerX = x;
         InnerY = y;
+
+        var placement = cont.RelativePortPlacement.Where(kv => kv.Value == this).Single().Key;
+        OuterX = placement.x;
+        OuterY = placement.y;
     }
 
     public State GetState() {
         return Grid.Get(InnerX, InnerY);
     }
 
+    public State[] GetNeighbors(IGrid parentGrid) {
+
+        int i = 0;
+        int gridWidth = Grid.Width;
+        int gridHeight = Grid.Height;
+
+        State[] neighbors = new State[5];
+
+        // walk neighbors
+        (int x, int y)[] offsets = new[] { (-1, 0), (1, 0), (-1, 0), (1, 0) };
+        foreach (var offset in offsets) {
+            int x = InnerX + offset.x;
+            int y = InnerY + offset.y;
+
+            bool isInside = x >= 0 && x < gridWidth && y >= 0 && y < gridHeight;
+
+            if (isInside) {
+                neighbors[i] = Grid.Get(x, y);
+            } else {
+                // TODO: rotations
+                neighbors[i] = parentGrid.Get(OuterX + offset.x, OuterY + offset.y);
+            }
+            i++;
+        }
+
+        neighbors[4] = GetState();
+
+        return neighbors;
+    }
+
     public int InnerX { get; }
     public int InnerY { get; }
+
+    public int OuterX { get; }
+    public int OuterY { get; }
+
+
 }
