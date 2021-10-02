7218d19784e74ff6731d38f6ecfd6d97ce1b9908
xiaoxiae <tomas@slama.dev>
2021-10-02 21:48:13 +0200

    stuff

--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -3,29 +3,44 @@ using UnityEngine.UI;
 
 public class PlayManager : MonoBehaviour {
     public bool Playing { get; private set; }
+    public Camera camera;
     public RectTransform backgroundPosition;
 
     [SerializeField] private Button buttonPrefab;
 
     private Button playPauseButton;
-    private Button DisableButton;
-
-    private int buttonSpacing = 8;
-    private float buttonSize;
+    private Button disableButton;
 
     private void Start() {
-        buttonSize = buttonPrefab.GetComponent<RectTransform>().rect.width;
-
         playPauseButton = Instantiate(buttonPrefab, transform);
         playPauseButton.onClick.AddListener(PlayPausePressed);
+        
+        disableButton = Instantiate(buttonPrefab, transform);
+        disableButton.onClick.AddListener(PlayPausePressed);
+    }
+
+    private void Update() {
+        float width = camera.scaledPixelWidth;
+        float height = camera.scaledPixelHeight;
+        
+        float c = 0.085f;
+        
+        float buttonSize = backgroundPosition.sizeDelta.y * 0.7f;
+        float buttonSpacing = buttonSize * 0.25f;
 
         RectTransform buttonTransform = playPauseButton.GetComponent<RectTransform>();
-
-        var yOffset = backgroundPosition.anchoredPosition.y - buttonSize / 2;
+        buttonTransform.sizeDelta = new Vector2(buttonSize, buttonSize);
+        buttonTransform.anchoredPosition = new Vector2(width - backgroundPosition.anchoredPosition.y - buttonSize / 2, backgroundPosition.anchoredPosition.y);
         
-        buttonTransform.anchoredPosition = new Vector2(-yOffset, yOffset);
+        buttonTransform = disableButton.GetComponent<RectTransform>();
+        buttonTransform.sizeDelta = new Vector2(buttonSize, buttonSize);
+        buttonTransform.anchoredPosition = new Vector2(width - backgroundPosition.anchoredPosition.y - buttonSize * (3/2) - buttonSpacing , backgroundPosition.anchoredPosition.y);
     }
 
     private void PlayPausePressed() {
+    }
+
+    private void DisablePressed() {
+        
     }
 }
