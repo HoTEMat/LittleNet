01b94a28442cd4fbf28b8ace37c8f0e19e6ace1d
xiaoxiae <tomas@slama.dev>
2021-10-02 15:37:25 +0200

    InputPort -> Port, +OutputPort

--- Assets/Scripts/Port.cs
+++ Assets/Scripts/Port.cs
@@ -1,3 +1,38 @@
+class InputPort : IPort {
+    public InputPort(int x, int y, ILevelValidator validator, int portPosition) {
+        Validator = validator;
+        PortPosition = portPosition;
+        InnerX = x;
+        InnerY = y;
+    }
+
+    public int InnerX { get; }
+    public int InnerY { get; }
+
+    private ILevelValidator Validator { get; }
+    private int PortPosition { get; }
+
+    public State GetState() => Validator.GetInputStates()[PortPosition];
+}
+
+class OutputPort : IPort {
+    private IGrid Grid;
+
+    public OutputPort(IGrid grid, int x, int y) {
+        Grid = grid;
+
+        InnerX = x;
+        InnerY = y;
+    }
+
+    public State GetState() {
+        return Grid.Get(InnerX, InnerY);
+    }
+
+    public int InnerX { get; }
+    public int InnerY { get; }
+}
+
 class Port : IPort {
     private IGrid Grid;
 
