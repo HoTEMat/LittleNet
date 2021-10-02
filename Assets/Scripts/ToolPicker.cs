7d69f5a738dfc5992ef20492c6dd73b63ade777e
xiaoxiae <tomas@slama.dev>
2021-10-02 20:58:24 +0200

    tool

--- Assets/Scripts/ToolPicker.cs
+++ Assets/Scripts/ToolPicker.cs
@@ -3,36 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
-using UnityEditor.PackageManager.UI;
 
 class ToolPicker : MonoBehaviour {
     public UITool CurrentTool { get; private set; }
     public Camera camera;
     public RectTransform backgroundPosition;
-
+    
     [SerializeField] private Button buttonPrefab;
 
     private List<Button> buttonPositions = new List<Button>();
 
-    private int buttonSpacing = 8;
-    private int buttonCount;
-    private float buttonSize;
-
     private void Start() {
-        int buttonNum = 0;
-        buttonSize = buttonPrefab.GetComponent<RectTransform>().rect.width;
-
-        buttonCount = 0;
-        foreach (State state in Enum.GetValues(typeof(State))) {
-            if (state.IsPlaceable())
-                buttonCount += 1;
-        }
-
-        float totalWidth = buttonSpacing * (buttonCount - 1) +
-                           buttonSize * buttonCount;
-
-        float leftOffset = (camera.scaledPixelWidth - totalWidth) / 2;
-
         foreach (State state in Enum.GetValues(typeof(State))) {
             if (!state.IsPlaceable())
                 continue;
@@ -43,12 +24,6 @@ class ToolPicker : MonoBehaviour {
             button.GetComponent<Image>().color = GridHolder.stateToColor[state];
 
             buttonPositions.Add(button);
-
-            RectTransform buttonTransform = button.GetComponent<RectTransform>();
-            float x = buttonNum * (buttonSize + buttonSpacing) + buttonSize / 2 + leftOffset;
-            buttonTransform.anchoredPosition = new Vector2(x, backgroundPosition.anchoredPosition.y - buttonSize / 2);
-
-            buttonNum++;
         }
     }
 
@@ -58,21 +33,34 @@ class ToolPicker : MonoBehaviour {
     }
 
     private void Update() {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
+        if (Input.GetKeyDown(KeyCode.Escape))
             CurrentTool = null;
-        }
+        
+        float width = camera.scaledPixelWidth;
+        float height = camera.scaledPixelHeight;
+        
+        float c = 0.085f;
+        
+        backgroundPosition.sizeDelta = new Vector2(width, height * c);
+        backgroundPosition.anchoredPosition = new Vector2(0, -height * c / 2);
+        
+        float buttonSize = backgroundPosition.sizeDelta.y * 0.7f;
+        float buttonSpacing = buttonSize * 0.25f;
 
-        float totalWidth = buttonSpacing * (buttonCount - 1) +
-                           buttonSize * buttonCount;
+        int buttonCount = buttonPositions.Count;
 
-        float leftOffset = (camera.scaledPixelWidth - totalWidth) / 2;
+        float toolsWidth = buttonSpacing * (buttonCount - 1) + buttonSize * buttonCount;
+
+        float leftOffset = (width - toolsWidth) / 2;
 
         int buttonNum = 0;
         foreach (Button button in buttonPositions) {
             RectTransform buttonTransform = button.GetComponent<RectTransform>();
 
             float x = buttonNum * (buttonSize + buttonSpacing) + buttonSize / 2 + leftOffset;
+            
             buttonTransform.anchoredPosition = new Vector2(x, backgroundPosition.anchoredPosition.y);
+            buttonTransform.sizeDelta = new Vector2(buttonSize, buttonSize);
 
             buttonNum++;
         }
