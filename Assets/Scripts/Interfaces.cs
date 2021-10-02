385f164900f1c9bd6b71e42ab7b8fb0323c7a65e
xiaoxiae <tomas@slama.dev>
2021-10-02 11:47:42 +0200

    more interface stuff

--- Assets/Scripts/Interfaces.cs
+++ Assets/Scripts/Interfaces.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 enum State {
+    Nothing,
     WireOn,
     WireDead,
     WireOff,
@@ -23,20 +24,45 @@ enum State {
     CrossHOffVDead,
 }
 
-interface IGridContainer {
+enum Rotation {
+    By0, By90, By180, By270
+}
+
+interface IGrid {
     State Get(int x, int y);
 
     void Set(int x, int y, State state);
 
+    void InsertContainer(IGridContainer container);
+    
+    // returns null when no container is found
+    IGridContainer GetContainerAt(int x, int y);
+    
+    void RemoveContainerAt(IGridContainer container);
+
+    List<IGridContainer> GetContainers();
+
+    // volá se jen na tom vnějším
+    void DoIteration();
+
     int Width { get; }
     int Height { get; }
+}
 
-    void DoIteration();
+interface IGridContainer {
+    int X { get; set; }
+    int Y { get; set; }
+
+    int OuterWidth { get; }
+    int OuterHeight { get; }
+
+    Rotation Rotation { get; set; }
+
+    IGrid Grid { get; }
 }
 
 interface IAutomaton {
     State NextState(State up, State down, State left, State right, State center);
 }
 
-public class Interfaces : MonoBehaviour {
-}
+public class Interfaces : MonoBehaviour { }
