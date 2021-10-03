49cf1b61a8a60c9d70a6339c53891549a534ac8b
Jan Kytka <kytka.honza@gmail.com>
2021-10-03 09:56:51 +0200

    calendar fix

---
+++ Assets/Scripts/Levels/CalendarValidator.cs
@@ -0,0 +1,44 @@
+﻿abstract class CalendarValidator : ILevelValidator {
+
+    protected delegate ILevelState DelayedValidator(State[] outputs);
+
+    SortedQueue<int, DelayedValidator> validators = new SortedQueue<int, DelayedValidator>();
+
+    public abstract int InputCount { get; }
+    public abstract int OutputCount { get; }
+    public int Iterations { get; private set; }
+
+    public abstract State[] GetInputStates();
+
+    public virtual void MoveToNextInputState() {
+        Iterations++;
+    }
+
+    public virtual void Reset() {
+        Iterations = 0;
+    }
+
+    protected void Expect(int after, DelayedValidator validator) {
+        validators.Enqueue(Iterations + after, validator);
+    }
+
+    protected void Succeed() {
+        validators.Enqueue(Iterations, outputs => ILevelState.Success);
+    }
+    protected void Fail() {
+        validators.Enqueue(Iterations, outputs => ILevelState.Failure);
+    }
+
+    public ILevelState ValidateStates(State[] outputs) {
+
+        while (validators.Count != 0 && Iterations >= validators.FirstKey) {
+            var validator = validators.Dequeue();
+            var result = validator.Invoke(outputs);
+            if (result != ILevelState.Nothing) {
+                return result;
+            }
+        }
+
+        return ILevelState.Nothing;
+    }
+}
